24d3ee793583279a2b6960e99bdbfd6db35ef390
Gavin M. Roy <gavinmroy@gmail.com>
2019-12-31 19:04:32 -0500

    Add working Channel 0
    Channel 0 is responsible for the "behind the scenes" communication with RabbitMQ.
    
    It is where the connection is negotiated, heartbeats are handled, connection.blocked and unblocked notifications are sent etc.
    
    As of this commit, this will negotiate a connection with RabbitMQ and handle all the various things that need to be handled.

--- aiorabbit/channel0.py
+++ aiorabbit/channel0.py
@@ -7,46 +7,47 @@ Handles all communication on Channel0
 """
 import asyncio
 import logging
-import sys
+import platform
 import typing
 
 from pamqp import commands, constants, frame, header, heartbeat
+from pamqp import exceptions as pamqp_exceptions
 
 from aiorabbit import exceptions, state, version
 
 LOGGER = logging.getLogger(__name__)
 
-_COMMANDS = typing.Union[commands.Connection.Blocked,
-                         commands.Connection.Unblocked,
-                         commands.Connection.Start,
-                         commands.Connection.Tune,
-                         commands.Connection.OpenOk,
-                         commands.Connection.Close,
-                         commands.Connection.CloseOk,
-                         heartbeat.Heartbeat]
+COMMANDS = typing.Union[commands.Connection.Blocked,
+                        commands.Connection.Unblocked,
+                        commands.Connection.Start,
+                        commands.Connection.Tune,
+                        commands.Connection.OpenOk,
+                        commands.Connection.Close,
+                        commands.Connection.CloseOk,
+                        heartbeat.Heartbeat]
+
+FRAMES: typing.Final[set] = {
+    'Connection.Blocked',
+    'Connection.Unblocked',
+    'Connection.Start',
+    'Connection.Tune',
+    'Connection.OpenOk',
+    'Connection.Close',
+    'Connection.CloseOk',
+    'Heartbeat'
+}
 
 
 class Channel0(state.StateManager):
 
-    FRAMES: typing.Final[set] = {
-        commands.Connection.Blocked,
-        commands.Connection.Unblocked,
-        commands.Connection.Start,
-        commands.Connection.Tune,
-        commands.Connection.OpenOk,
-        commands.Connection.Close,
-        commands.Connection.CloseOk,
-        heartbeat.Heartbeat
-    }
-
     STATE_UNINITIALIZED: typing.Final[int] = 0x00
     STATE_PROTOCOL_HEADER_SENT: typing.Final[int] = 0x01
-    STATE_OPEN_SENT: typing.Final[int] = 0x02
-    STATE_OPEN_OK_RECEIVED: typing.Final[int] = 0x03
-    STATE_START_RECEIVED: typing.Final[int] = 0x04
-    STATE_START_OK_SENT: typing.Final[int] = 0x05
-    STATE_TUNE_RECEIVED: typing.Final[int] = 0x06
-    STATE_TUNE_OK_SENT: typing.Final[int] = 0x07
+    STATE_START_RECEIVED: typing.Final[int] = 0x02
+    STATE_START_OK_SENT: typing.Final[int] = 0x03
+    STATE_TUNE_RECEIVED: typing.Final[int] = 0x04
+    STATE_TUNE_OK_SENT: typing.Final[int] = 0x05
+    STATE_OPEN_SENT: typing.Final[int] = 0x06
+    STATE_OPEN_OK_RECEIVED: typing.Final[int] = 0x07
     STATE_HEARTBEAT_RECEIVED: typing.Final[int] = 0x08
     STATE_HEARTBEAT_SENT: typing.Final[int] = 0x09
     STATE_CLOSE_RECEIVED: typing.Final[int] = 0x10
@@ -56,9 +57,9 @@ class Channel0(state.StateManager):
     STATE_UNBLOCKED_RECEIVED: typing.Final[int] = 0x14
     STATE_CLOSED: typing.Final[int] = 0x15
 
-    STATE: typing.Final[dict] = {
+    STATE_MAP: typing.Final[dict] = {
         0x00: 'Uninitialized',
-        0x01: 'Sending Protocol Header',
+        0x01: 'Protocol Header Received',
         0x02: 'Start Received',
         0x03: 'StartOk Sent',
         0x04: 'Tune Received',
@@ -76,15 +77,36 @@ class Channel0(state.StateManager):
     }
 
     STATE_TRANSITIONS: typing.Final[dict] = {
-        STATE_UNINITIALIZED: {STATE_PROTOCOL_HEADER_SENT},
-        STATE_PROTOCOL_HEADER_SENT: {STATE_OPEN_SENT, STATE_CLOSED},
-        STATE_START_RECEIVED: {STATE_CLOSED, STATE_START_OK_SENT},
-        STATE_START_OK_SENT: {STATE_TUNE_RECEIVED, STATE_CLOSED},
-        STATE_TUNE_RECEIVED: {STATE_TUNE_OK_SENT},
-        STATE_TUNE_OK_SENT: {STATE_OPEN_SENT},
-        STATE_OPEN_SENT: {STATE_OPEN_OK_RECEIVED},
-        STATE_OPEN_OK_RECEIVED: {STATE_START_RECEIVED, STATE_CLOSED},
-        STATE_CLOSE_RECEIVED: {STATE_CLOSE_OK_SENT, STATE_CLOSED},
+        STATE_UNINITIALIZED: {
+            STATE_PROTOCOL_HEADER_SENT},
+        STATE_PROTOCOL_HEADER_SENT: {
+            STATE_START_RECEIVED,
+            STATE_CLOSED},
+        STATE_START_RECEIVED: {
+            STATE_START_OK_SENT,
+            STATE_CLOSED},
+        STATE_START_OK_SENT: {
+            STATE_TUNE_RECEIVED,
+            STATE_CLOSED},
+        STATE_TUNE_RECEIVED: {
+            STATE_TUNE_OK_SENT},
+        STATE_TUNE_OK_SENT: {
+            STATE_OPEN_SENT},
+        STATE_OPEN_SENT: {
+            STATE_OPEN_OK_RECEIVED},
+        STATE_OPEN_OK_RECEIVED: {
+            STATE_BLOCKED_RECEIVED,
+            STATE_HEARTBEAT_RECEIVED,
+            STATE_CLOSE_RECEIVED,
+            STATE_CLOSE_SENT,
+            STATE_CLOSED},
+        STATE_CLOSE_RECEIVED: {
+            STATE_CLOSE_OK_SENT,
+            STATE_CLOSED},
+        STATE_CLOSE_SENT: {
+            STATE_CLOSED},
+        STATE_CLOSE_OK_SENT: {
+            STATE_CLOSED},
         STATE_BLOCKED_RECEIVED: {
             STATE_UNBLOCKED_RECEIVED,
             STATE_CLOSE_RECEIVED,
@@ -103,71 +125,84 @@ class Channel0(state.StateManager):
         STATE_HEARTBEAT_SENT: {
             STATE_HEARTBEAT_RECEIVED,
             STATE_BLOCKED_RECEIVED,
-            STATE_CLOSE_RECEIVED,
             STATE_UNBLOCKED_RECEIVED,
+            STATE_CLOSE_RECEIVED,
+            STATE_CLOSE_SENT,
             STATE_CLOSED},
-        STATE_CLOSED: {STATE_PROTOCOL_HEADER_SENT}
+        STATE_CLOSED: {
+            STATE_PROTOCOL_HEADER_SENT}
     }
 
     def __init__(self,
-                 virtual_host: str,
-                 heartbeat_interval: typing.Optional[int],
-                 product: str,
+                 blocked: asyncio.Event,
                  username: str,
                  password: str,
+                 virtual_host: str,
+                 heartbeat_interval: typing.Optional[int],
                  locale: str,
                  loop: asyncio.AbstractEventLoop,
-                 blocked: asyncio.Event,
-                 connected: asyncio.Event,
-                 writer: asyncio.StreamWriter):
+                 max_channels: int,
+                 product: str):
         super().__init__(loop)
-        self._blocked = blocked
-        self._connected = connected
-        self._heartbeat_interval = heartbeat_interval
-        self._locale = locale
-        self._password = password
-        self._product = product
-        self._properties: dict = {}
-        self._username = username
-        self._virtual_host = virtual_host
-        self._writer = writer
+        self.blocked = blocked
+        self.locale = locale
+        self.password = password
+        self.product = product
+        self.properties: dict = {}
+        self.transport: typing.Optional[asyncio.Transport] = None
+        self.username = username
+        self.virtual_host = virtual_host
+        self.heartbeat_interval = heartbeat_interval
         self.max_frame_size = constants.FRAME_MAX_SIZE
-        self.max_channels = 32768  # Max signed short integer value
+        self.max_channels = max_channels
 
-    async def process(self, value: _COMMANDS) -> typing.NoReturn:
+    def process(self, value: COMMANDS) -> None:
+        LOGGER.debug('Processing %r', value)
         if isinstance(value, commands.Connection.Start):
             self._set_state(self.STATE_START_RECEIVED)
-            await self._process_start(value)
+            self._process_start(value)
         elif isinstance(value, commands.Connection.Tune):
             self._set_state(self.STATE_TUNE_RECEIVED)
-            await self._process_tune(value)
+            self._process_tune(value)
         elif isinstance(value, commands.Connection.OpenOk):
             self._set_state(self.STATE_OPEN_OK_RECEIVED)
-            self._connected.set()
         elif isinstance(value, commands.Connection.Blocked):
             self._set_state(self.STATE_BLOCKED_RECEIVED)
-            self._blocked.set()
+            self.blocked.set()
         elif isinstance(value, commands.Connection.Unblocked):
             self._set_state(self.STATE_UNBLOCKED_RECEIVED)
-            self._blocked.clear()
+            self.blocked.clear()
         elif isinstance(value, commands.Connection.Close):
             self._set_state(self.STATE_CLOSE_RECEIVED)
-            await self._process_close(value)
+            self._process_close(value)
         elif isinstance(value, commands.Connection.CloseOk):
             self._set_state(self.STATE_CLOSED)
-            self._connected.clear()
         elif isinstance(value, heartbeat.Heartbeat):
             self._set_state(self.STATE_HEARTBEAT_RECEIVED)
-            await self._writer.write(frame.marshal(heartbeat.Heartbeat(), 0))
+            self.transport.write(frame.marshal(heartbeat.Heartbeat(), 0))
             self._set_state(self.STATE_HEARTBEAT_SENT)
+        else:
+            self._exception = exceptions.AIORabbitException(
+                'Unsupported Frame Passed to Channel0')
+        if self._exception:
+            raise self._exception
 
-    async def open(self):
-        await self._writer.write(frame.marshal(header.ProtocolHeader(), 0))
-        self._set_state(self.STATE_OPEN_SENT)
+    async def open(self, transport: asyncio.Transport) -> None:
+        self.transport = transport
+        self.transport.write(frame.marshal(header.ProtocolHeader(), 0))
+        self._set_state(self.STATE_PROTOCOL_HEADER_SENT)
+        await self._wait_on_state(
+            self.STATE_OPEN_OK_RECEIVED, self.STATE_CLOSED)
 
-    async def close(self):
-        await self._writer.write(frame.marshal(commands.Connection.Close(), 0))
-        self._set_state(self.STATE_CLOSE_OK_SENT)
+    async def close(self, code=200):
+        self.transport.write(frame.marshal(
+            commands.Connection.Close(code, 'Client Requested', 0, 0), 0))
+        self._set_state(self.STATE_CLOSE_SENT)
+        await self._wait_on_state(self.STATE_CLOSED)
+
+    @property
+    def is_closed(self):
+        return self._state == self.STATE_CLOSED
 
     @staticmethod
     def _negotiate(client: int, server: int) -> int:
@@ -177,29 +212,34 @@ class Channel0(state.StateManager):
         """
         return min(client, server) or (client or server)
 
-    async def _process_close(self, value: commands.Connection.Close) \
-            -> typing.NoReturn:
+    def _process_close(self, value: commands.Connection.Close) -> None:
         LOGGER.warning('RabbitMQ closed the connection (%s): %s',
                        value.reply_code, value.reply_text)
-        await self._writer.write(frame.marshal(
-            commands.Connection.CloseOk(), 0))
-        if value.reply_code in exceptions.CLASS_MAPPING:
-            raise exceptions.CLASS_MAPPING[value.reply_code](value.reply_text)
+        self.transport.write(frame.marshal(commands.Connection.CloseOk(), 0))
+        if value.reply_code < 300:
+            self._set_state(self.STATE_CLOSE_OK_SENT)
+        elif value.reply_code in pamqp_exceptions.CLASS_MAPPING:
+            self._set_state(
+                self.STATE_CLOSE_OK_SENT,
+                pamqp_exceptions.CLASS_MAPPING[value.reply_code](
+                    value.reply_text))
         else:
-            raise exceptions.ConnectionClosedException(
-                value.reply_code, value.reply_text)
+            self._set_state(
+                self.STATE_CLOSE_OK_SENT,
+                exceptions.ConnectionClosedException(
+                    value.reply_code, value.reply_text))
 
-    async def _process_start(self, value: commands.Connection.Start) \
-            -> typing.NoReturn:
+    def _process_start(self, value: commands.Connection.Start) -> None:
         if (value.version_major,
             value.version_minor) != (constants.VERSION[0],
                                      constants.VERSION[1]):
             LOGGER.warning('AMQP version error (received %i.%i, expected %r)',
                            value.version_major, value.version_minor,
                            constants.VERSION)
-            self._writer.close()
-            self._set_state(self.STATE_CLOSED)
-            raise exceptions.ClientNegotiationException()
+            self.transport.close()
+            self._set_state(
+                self.STATE_CLOSED, exceptions.ClientNegotiationException())
+            return
 
         self.properties = dict(value.server_properties)
         for key in self.properties:
@@ -209,24 +249,26 @@ class Channel0(state.StateManager):
                                  capability, self.properties[key][capability])
             else:
                 LOGGER.debug('Server %s: %r', key, self.properties[key])
-        await self._writer.write(frame.marshal(
+        self.transport.write(frame.marshal(
             commands.Connection.StartOk(
                 client_properties={
-                    'product': self._product,
-                    'platform': 'Python {0}.{1}.{2}'.format(*sys.version_info),
+                    'product': self.product,
+                    'platform': 'Python {}'.format(platform.python_version()),
                     'capabilities': {'authentication_failure_close': True,
                                      'basic.nack': True,
                                      'connection.blocked': True,
                                      'consumer_cancel_notify': True,
+                                     'consumer_priorities': True,
+                                     'direct_reply_to': True,
+                                     'per_consumer_qos': True,
                                      'publisher_confirms': True},
                     'information': 'See https://aiorabbit.readthedocs.io',
                     'version': version},
-                response='\0{}\0{}'.format(self._username, self._password),
-                locale=self._locale), 0))
+                response='\0{}\0{}'.format(self.username, self.password),
+                locale=self.locale), 0))
         self._set_state(self.STATE_START_OK_SENT)
 
-    async def _process_tune(self, value: commands.Connection.Tune) \
-            -> typing.NoReturn:
+    def _process_tune(self, value: commands.Connection.Tune) -> None:
         self.max_channels = self._negotiate(
             self.max_channels, value.channel_max)
         self.max_frame_size = self._negotiate(
@@ -235,11 +277,11 @@ class Channel0(state.StateManager):
             self.heartbeat_interval = value.heartbeat
         elif not self.heartbeat_interval and not value.heartbeat:
             self.heartbeat_interval = 0
-        await self._writer.write(frame.marshal(
+        self.transport.write(frame.marshal(
             commands.Connection.TuneOk(
                 self.max_channels, self.max_frame_size,
                 self.heartbeat_interval), 0))
-        self._set_state(self.STATE_START_OK_SENT)
-        await self._writer.write(
-            frame.marshal(commands.Connection.Open(self._virtual_host), 0))
+        self._set_state(self.STATE_TUNE_OK_SENT)
+        self.transport.write(
+            frame.marshal(commands.Connection.Open(self.virtual_host), 0))
         self._set_state(self.STATE_OPEN_SENT)
---
+++ tests/test_channel0.py
@@ -0,0 +1,208 @@
+import asyncio
+import logging
+import platform
+import typing
+import unittest
+from unittest import mock
+import uuid
+
+from pamqp import commands, constants, frame, heartbeat
+from pamqp import exceptions as pamqp_exceptions
+
+from aiorabbit import channel0, exceptions, version
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TestCase(unittest.TestCase):
+
+    HEARTBEAT_INTERVAL = 10
+    SERVER_HEARTBEAT_INTERVAL = 30
+    MAX_CHANNELS = 256
+    SERVER_MAX_CHANNELS = 32768
+
+    def setUp(self):
+        self.blocked = asyncio.Event()
+        self.username = str(uuid.uuid4())
+        self.password = str(uuid.uuid4())
+        self.locale = str(uuid.uuid4())
+        self.product = str(uuid.uuid4())
+        self.virtual_host = '/'
+        self.heartbeat = asyncio.Event()
+        self.loop = asyncio.get_event_loop()
+        self.server_properties = {
+            'capabilities': {'authentication_failure_close': True,
+                             'basic.nack': True,
+                             'connection.blocked': True,
+                             'consumer_cancel_notify': True,
+                             'consumer_priorities': True,
+                             'direct_reply_to': True,
+                             'per_consumer_qos': True,
+                             'publisher_confirms': True},
+            'cluster_name': 'mock@{}'.format(str(uuid.uuid4())),
+            'platform': 'Python {}'.format(platform.python_version()),
+            'production': 'aiorabbit',
+            'version': version
+        }
+        self.transport = mock.create_autospec(asyncio.Transport)
+        self.transport.write = self._transport_write
+        self.channel0 = channel0.Channel0(
+            self.blocked, self.username, self.password, self.virtual_host,
+            self.HEARTBEAT_INTERVAL, self.locale, self.loop, self.MAX_CHANNELS,
+            self.product)
+
+    def _connection_start(self):
+        self.channel0.process(
+            commands.Connection.Start(
+                server_properties=self.server_properties))
+
+    def _connection_tune(self):
+        self.channel0.process(
+            commands.Connection.Tune(
+                self.SERVER_MAX_CHANNELS, constants.FRAME_MAX_SIZE,
+                self.SERVER_HEARTBEAT_INTERVAL))
+
+    def _connection_open_ok(self):
+        self.channel0.process(commands.Connection.OpenOk())
+
+    def _connection_close_ok(self):
+        self.channel0.process(commands.Connection.CloseOk())
+
+    def _transport_write(self, value: bytes) -> typing.NoReturn:
+        count, channel, frame_value = frame.unmarshal(value)
+        self.assertEqual(count, len(value), 'All bytes used')
+        self.assertEqual(channel, 0, 'Frame was published on channel 0')
+        if frame_value.name == 'ProtocolHeader':
+            self.loop.call_soon(self._connection_start)
+        elif frame_value.name == 'Connection.StartOk':
+            self.loop.call_soon(self._connection_tune)
+        elif frame_value.name == 'Connection.TuneOk':
+            pass
+        elif frame_value.name == 'Connection.Open':
+            self.loop.call_soon(self._connection_open_ok)
+        elif frame_value.name == 'Connection.Close':
+            self.loop.call_soon(self._connection_close_ok)
+        elif frame_value.name == 'Connection.CloseOk':
+            pass
+        elif frame_value.name == 'Heartbeat':
+            self.heartbeat.set()
+        else:
+            raise RuntimeError(count, channel, frame_value)
+
+    async def open(self):
+        self.assert_state(self.channel0.STATE_UNINITIALIZED)
+        await self.channel0.open(self.transport)
+
+    def assert_state(self, state):
+        self.assertEqual(self.channel0.state, self.channel0.STATE_MAP[state])
+
+    def test_negotiation(self):
+        self.loop.run_until_complete(self.open())
+
+
+class ProtocolMismatchTestCase(TestCase):
+
+    def _connection_start(self):
+        self.channel0.process(
+            commands.Connection.Start(
+                version_major=1, version_minor=0,
+                server_properties=self.server_properties))
+
+    def test_negotiation(self):
+        with self.assertRaises(exceptions.ClientNegotiationException):
+            self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_CLOSED)
+
+
+class RemoteCloseTestCase(TestCase):
+
+    def test_with_remote_200(self):
+        self.loop.run_until_complete(self.open())
+        self.channel0.process(commands.Connection.Close(200, 'OK'))
+        self.assert_state(self.channel0.STATE_CLOSE_OK_SENT)
+
+    def test_with_fake_code(self):
+        self.loop.run_until_complete(self.open())
+        with self.assertRaises(exceptions.ConnectionClosedException):
+            self.channel0.process(
+                commands.Connection.Close(999, 'Error', 0, 0))
+        self.assert_state(self.channel0.STATE_CLOSE_OK_SENT)
+
+    def test_with_invalid_path(self):
+        self.loop.run_until_complete(self.open())
+        with self.assertRaises(pamqp_exceptions.AMQPInvalidPath):
+            self.channel0.process(
+                commands.Connection.Close(402, 'INVALID-PATH'))
+        self.assert_state(self.channel0.STATE_CLOSE_OK_SENT)
+
+
+class ClientCloseTestCase(TestCase):
+
+    def test_close(self):
+        self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_OPEN_OK_RECEIVED)
+        self.loop.run_until_complete(self.channel0.close())
+        self.assert_state(self.channel0.STATE_CLOSED)
+
+
+class ConnectionBlockedTestCase(TestCase):
+
+    def test_block_unblock(self):
+        self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_OPEN_OK_RECEIVED)
+        self.channel0.process(commands.Connection.Blocked())
+        self.assert_state(self.channel0.STATE_BLOCKED_RECEIVED)
+        self.assertTrue(self.channel0.blocked.is_set())
+        self.channel0.process(commands.Connection.Unblocked())
+        self.assert_state(self.channel0.STATE_UNBLOCKED_RECEIVED)
+        self.assertFalse(self.channel0.blocked.is_set())
+
+
+class HeartbeatTestCase(TestCase):
+
+    def test_heartbeat(self):
+        self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_OPEN_OK_RECEIVED)
+        self.channel0.process(heartbeat.Heartbeat())
+        self.assert_state(self.channel0.STATE_HEARTBEAT_SENT)
+        self.assertTrue(self.heartbeat.is_set())
+
+
+class NoHeartbeatTestCase(TestCase):
+
+    HEARTBEAT_INTERVAL = 0
+    SERVER_HEARTBEAT_INTERVAL = 0
+
+    def test_negotiated_interval(self):
+        self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_OPEN_OK_RECEIVED)
+        self.assertEqual(self.channel0.heartbeat_interval, 0)
+
+
+class NoClientHeartbeatTestCase(TestCase):
+
+    HEARTBEAT_INTERVAL = None
+    SERVER_HEARTBEAT_INTERVAL = 0
+
+    def test_negotiated_interval(self):
+        self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_OPEN_OK_RECEIVED)
+        self.assertEqual(self.channel0.heartbeat_interval, 0)
+
+
+class SmallerClientHeartbeatTestCase(TestCase):
+
+    HEARTBEAT_INTERVAL = 10
+    SERVER_HEARTBEAT_INTERVAL = 30
+
+    def test_negotiated_interval(self):
+        self.loop.run_until_complete(self.open())
+        self.assert_state(self.channel0.STATE_OPEN_OK_RECEIVED)
+        self.assertEqual(self.channel0.heartbeat_interval, 10)
+
+
+class InvalidFrameTestCase(TestCase):
+
+    def test_negotiated_interval(self):
+        with self.assertRaises(exceptions.AIORabbitException):
+            self.channel0.process(commands.Basic.Cancel('foo'))
