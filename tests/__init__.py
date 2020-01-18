c110810dc1f89f224b0163a6be8e70588a9c1d62
Gavin M. Roy <gavinmroy@gmail.com>
2020-01-18 15:38:39 -0500

    Create AsyncTestCase and async_test decorator
    Will provide an easier way to write tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,9 @@
+import asyncio
+import functools
 import os
+import unittest
+
+from aiorabbit import client
 
 
 def setup_module():
@@ -12,3 +17,44 @@ def setup_module():
                 os.environ[name] = value
     except IOError:
         pass
+
+
+def async_test(*func):
+    if func:
+        @functools.wraps(func[0])
+        def wrapper(*args, **kwargs):
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(func[0](*args, **kwargs))
+        return wrapper
+
+
+class AsyncTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.loop = asyncio.get_event_loop()
+        self.rabbitmq_url = os.environ['RABBITMQ_URI']
+        self.client = client.Client(self.rabbitmq_url, loop=self.loop)
+        self.timeout = int(os.environ.get('ASYNC_TIMEOUT', '3'))
+        self.timeout_handle = self.loop.call_later(
+            self.timeout, self.on_timeout)
+
+    def tearDown(self):
+        if not self.timeout_handle.cancelled():
+            self.timeout_handle.cancel()
+
+    def assert_state(self, state):
+        self.assertEqual(self.client.state, self.client.STATE_MAP[state])
+
+    def on_timeout(self):
+        self.loop.stop()
+        raise TimeoutError(
+            'Test duration exceeded {} seconds'.format(self.timeout))
+
+    async def connect(self):
+        self.assert_state(self.client.STATE_DISCONNECTED)
+        await self.client.connect()
+        self.assert_state(self.client.STATE_CHANNEL_OPENOK_RECEIVED)
+
+    async def close(self):
+        await self.client.close()
+        self.assert_state(self.client.STATE_CLOSED)
