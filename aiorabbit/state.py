c0c7513e165731e445e3fbfe88d15d7b6a94f595
Gavin M. Roy <gavinmroy@gmail.com>
2020-01-18 17:55:02 -0500

    State pattern changes and cleanup

--- aiorabbit/state.py
+++ aiorabbit/state.py
@@ -10,16 +10,17 @@ import typing
 
 from aiorabbit import exceptions
 
+STATE_UNINITIALIZED = 0x00
+STATE_EXCEPTION = 0x01
+
 
 class StateManager:
     """Base Class used to implement state management"""
-
-    STATE_UNINITIALIZED = 0x00
-    STATE_EXCEPTION = 0x01
     STATE_MAP: dict = {
         STATE_UNINITIALIZED: 'Uninitialized',
         STATE_EXCEPTION: 'Exception Raised'
     }
+
     STATE_TRANSITIONS: dict = {
         STATE_UNINITIALIZED: [STATE_EXCEPTION]
     }
@@ -30,48 +31,54 @@ class StateManager:
         self._exception: typing.Optional[Exception] = None
         self._loop: asyncio.AbstractEventLoop = loop
         self._loop.set_exception_handler(self._on_exception)
-        self._state: int = self.STATE_UNINITIALIZED
+        self._state: int = STATE_UNINITIALIZED
         self._state_start: float = self._loop.time()
         self._waits: dict = {}
 
     @property
     def state(self) -> str:
         """Return the current state as descriptive string"""
-        return self.STATE_MAP[self._state]
+        return self.state_description(self._state)
+
+    def state_description(self, state: int) -> str:
+        """Return a state description for a given state"""
+        return self.STATE_MAP[state]
+
+    @property
+    def time_in_state(self) -> float:
+        """Return how long the current state has been active"""
+        return self._loop.time() - self._state_start
 
     def _on_exception(self,
                       _loop: asyncio.AbstractEventLoop,
                       context: typing.Dict[str, typing.Any]) -> None:
         self._logger.debug('Exception on IOLoop: %r', context)
-        self._set_state(self.STATE_EXCEPTION, context['exception'])
+        self._set_state(STATE_EXCEPTION, context['exception'])
 
-    def _set_state(self, new_state: int,
+    def _set_state(self, value: int,
                    exc: typing.Optional[Exception] = None) -> None:
-        self._logger.debug('Set state: %r [%r] - Current: %r',
-                           self.STATE_MAP[new_state], exc, self.state)
-        if new_state == self._state:
-            self._logger.debug('Ignoring state reassignment (%r)', self.state)
-            return
-        elif new_state not in self.STATE_TRANSITIONS[self._state] \
-                and new_state != self.STATE_EXCEPTION:
-            raise exceptions.StateTransitionError(
-                'Invalid state transition from {!r} to {!r}'.format(
-                    self.state, self.STATE_MAP[new_state]))
-        self._logger.debug('Transition to %s after %.4f seconds',
-                           self.STATE_MAP[new_state],
-                           self._loop.time() - self._state_start)
-        if exc:
+        self._logger.debug('state: %r (%r) [%r], value: %r (%r) [%r]',
+                           self.state, self._state, self._exception,
+                           self.state_description(value), value, exc)
+        if value == STATE_EXCEPTION or exc:
             self._exception = exc
-            if new_state != self.STATE_EXCEPTION:
-                new_state = self.STATE_EXCEPTION
-                self._logger.debug('Forcing exception state instead of %r',
-                                   self.STATE_MAP[new_state])
-        self._state = new_state
-        self._state_start = self._loop.time()
+            self._state = STATE_EXCEPTION
+        else:
+            if value == self._state:
+                raise exceptions.StateTransitionError(
+                    'Attempted to reassign the same state: {!r}'.format(
+                        self.state))
+            elif value not in self.STATE_TRANSITIONS[self._state]:
+                raise exceptions.StateTransitionError(
+                    'Invalid state transition from {!r} to {!r}'.format(
+                        self.state, self.state_description(value)))
+            else:
+                self._logger.debug('Transition to %s after %.4f seconds',
+                                   self.STATE_MAP[value], self.time_in_state)
+                self._state = value
+                self._state_start = self._loop.time()
         if self._state in self._waits:
             self._waits[self._state].set()
-        self._logger.debug('Current state: %r [%r]',
-                           self.state, self.exception)
 
     @property
     def exception(self) -> typing.Optional[Exception]:
--- tests/test_state.py
+++ tests/test_state.py
@@ -1,74 +1,72 @@
 import asyncio
-import unittest
 
 from aiorabbit import exceptions, state
+
+from . import testing
+
+STATE_FOO = 0x10
+STATE_BAR = 0x11
+STATE_BAZ = 0x12
 
 
 class Test(state.StateManager):
 
-    STATE_UNINITIALIZED = 0x00
-    STATE_FOO = 0x01
-    STATE_BAR = 0x02
-    STATE_BAZ = 0x03
-
     STATE_MAP = {
-        STATE_UNINITIALIZED: 'Uninitialized',
+        state.STATE_UNINITIALIZED: 'Uninitialized',
+        state.STATE_EXCEPTION: 'Exception',
         STATE_FOO: 'Foo',
         STATE_BAR: 'Bar',
         STATE_BAZ: 'Baz',
 
     }
     STATE_TRANSITIONS = {
-        STATE_UNINITIALIZED: {STATE_FOO, STATE_BAR},
-        STATE_FOO: {STATE_BAR},
-        STATE_BAR: {STATE_BAZ},
-        STATE_BAZ: {STATE_FOO}
+        state.STATE_UNINITIALIZED: [STATE_FOO, STATE_BAR],
+        state.STATE_EXCEPTION: [],
+        STATE_FOO: [STATE_BAR],
+        STATE_BAR: [STATE_BAZ],
+        STATE_BAZ: [STATE_FOO]
     }
 
     def set_state(self, value: int) -> None:
         self._set_state(value)
 
 
-class TestCase(unittest.TestCase):
+class TestCase(testing.AsyncTestCase):
 
     def setUp(self) -> None:
-        self.loop = asyncio.get_event_loop()
+        super().setUp()
         self.obj = Test(self.loop)
 
     def assert_state(self, value):
         self.assertEqual(self.obj.state, self.obj.STATE_MAP[value])
 
     def test_state_transitions(self):
-        self.assert_state(self.obj.STATE_UNINITIALIZED)
-        self.obj.set_state(self.obj.STATE_FOO)
-        self.assert_state(self.obj.STATE_FOO)
-        self.obj.set_state(self.obj.STATE_BAR)
-        self.assert_state(self.obj.STATE_BAR)
-        self.obj.set_state(self.obj.STATE_BAZ)
-        self.assert_state(self.obj.STATE_BAZ)
-        self.obj.set_state(self.obj.STATE_FOO)
-        self.assert_state(self.obj.STATE_FOO)
+        self.assert_state(state.STATE_UNINITIALIZED)
+        self.obj.set_state(STATE_FOO)
+        self.assert_state(STATE_FOO)
+        self.obj.set_state(STATE_BAR)
+        self.assert_state(STATE_BAR)
+        self.obj.set_state(STATE_BAZ)
+        self.assert_state(STATE_BAZ)
+        self.obj.set_state(STATE_FOO)
+        self.assert_state(STATE_FOO)
 
     def test_invalid_state_transition(self):
-        self.assert_state(self.obj.STATE_UNINITIALIZED)
+        self.assert_state(state.STATE_UNINITIALIZED)
         with self.assertRaises(exceptions.StateTransitionError):
-            self.obj.set_state(self.obj.STATE_BAZ)
+            self.obj.set_state(STATE_BAZ)
 
     def test_setting_state_to_same_value(self):
-        self.assert_state(self.obj.STATE_UNINITIALIZED)
-        self.obj.set_state(self.obj.STATE_FOO)
-        state_start = self.obj._state_start
-        self.assert_state(self.obj.STATE_FOO)
-        self.obj.set_state(self.obj.STATE_FOO)
-        self.assertEqual(state_start, self.obj._state_start)
+        self.assert_state(state.STATE_UNINITIALIZED)
+        self.obj.set_state(STATE_FOO)
+        self.assert_state(STATE_FOO)
+        with self.assertRaises(exceptions.StateTransitionError):
+            self.obj.set_state(STATE_FOO)
 
-    def test_wait_on_state(self):
-
-        async def run_test():
-            self.loop.call_soon(self.obj.set_state, self.obj.STATE_FOO)
-            await self.obj._wait_on_state(self.obj.STATE_FOO)
-            self.loop.call_soon(self.obj.set_state, self.obj.STATE_BAR)
-            await self.obj._wait_on_state(self.obj.STATE_BAR)
-
-        self.loop.run_until_complete(asyncio.ensure_future(run_test()))
-        self.assert_state(self.obj.STATE_BAR)
+    @testing.async_test
+    async def run_test(self):
+        self.loop.call_soon(self.obj.set_state, STATE_FOO)
+        await self.obj._wait_on_state(STATE_FOO)
+        self.loop.call_soon(self.obj.set_state, STATE_BAR)
+        await self.obj._wait_on_state(STATE_BAR)
+        self.assert_state(STATE_BAR)
