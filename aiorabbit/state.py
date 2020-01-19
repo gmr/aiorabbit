2b271c8b01e4718b3ab9fd79987616b7c8b6c41a
Gavin M. Roy <gavinmroy@gmail.com>
2020-01-18 22:35:46 -0500

    Add sticky state
    Used to keep track of when assembling messages

--- aiorabbit/state.py
+++ aiorabbit/state.py
@@ -34,7 +34,13 @@ class StateManager:
         self._loop.set_exception_handler(self._on_exception)
         self._state: int = STATE_UNINITIALIZED
         self._state_start: float = self._loop.time()
+        self._sticky_state = set({})
         self._waits: dict = {}
+
+    @property
+    def exception(self) -> typing.Optional[Exception]:
+        """If an exception was set with the state, return the value"""
+        return self._exception
 
     @property
     def state(self) -> str:
@@ -50,6 +56,12 @@ class StateManager:
         """Return how long the current state has been active"""
         return self._loop.time() - self._state_start
 
+    def _clear_sticky_state(self, value: int) -> None:
+        try:
+            self._sticky_state.remove(value)
+        except KeyError:
+            pass
+
     def _on_exception(self,
                       _loop: asyncio.AbstractEventLoop,
                       context: typing.Dict[str, typing.Any]) -> None:
@@ -57,11 +69,10 @@ class StateManager:
         self._set_state(STATE_EXCEPTION, context['exception'])
 
     def _set_state(self, value: int,
-                   exc: typing.Optional[Exception] = None) -> None:
-        self._logger.debug('state: %r (%r) [%r], value: %r (%r) [%r]',
-                           self.state, self._state, self._exception,
-                           self.state_description(value), value, exc)
+                   exc: typing.Optional[Exception] = None,
+                   sticky: bool = False) -> None:
         if value == STATE_EXCEPTION or exc:
+            self._logger.debug('Exception passed in with state: %r', exc)
             self._exception = exc
             self._state = STATE_EXCEPTION
         else:
@@ -72,16 +83,16 @@ class StateManager:
                     'Invalid state transition from {!r} to {!r}'.format(
                         self.state, self.state_description(value)))
             else:
-                self._logger.debug('Transition to %s after %.4f seconds',
-                                   self.STATE_MAP[value], self.time_in_state)
+                self._logger.debug(
+                    'Transition to %r (%i) from %r (%i) after %.4f seconds '
+                    '(Sticky: %r)', self.state_description(value), value,
+                    self.state, self._state, self.time_in_state, sticky)
                 self._state = value
                 self._state_start = self._loop.time()
+                if sticky:
+                    self._sticky_state.add(value)
         if self._state in self._waits:
             self._waits[self._state].set()
-
-    @property
-    def exception(self) -> typing.Optional[Exception]:
-        return self._exception
 
     async def _wait_on_state(self, *args) -> None:
         """Wait on a specific state value to transition"""
