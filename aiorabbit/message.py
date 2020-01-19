40b8e6c6315e039047402b22973f696b3ffb47e6
Gavin M. Roy <gavinmroy@gmail.com>
2020-01-18 21:21:42 -0500

    Add new Message class
    Is used to represent a message sent from RabbitMQ to the client via Basic.Deliver, Basic.GetOk, and Basic.Return

---
+++ aiorabbit/message.py
@@ -0,0 +1,165 @@
+"""
+Received Messages
+=================
+
+"""
+import datetime
+import logging
+import typing
+
+from pamqp import body, commands, header
+
+LOGGER = logging.getLogger(__name__)
+METHODS = [commands.Basic.Deliver, commands.Basic.GetOk, commands.Basic.Return]
+
+
+class Message:
+    """Represents a message received from RabbitMQ"""
+    def __init__(self, method: METHODS) -> None:
+        self.method = method
+        self.header: typing.Optional[header.ContentHeader] = None
+        self.body_frames: typing.List[body.ContentBody] = []
+
+    def __bytes__(self):
+        """Return the message body if the instance is accessed using
+        ``bytes(Message)``
+
+        """
+        return self.body
+
+    def __len__(self):
+        """Return the length of the message body"""
+        return len(self.body)
+
+    @property
+    def consumer_tag(self) -> typing.Optional[str]:
+        """If the delivered, the consumer tag it was delivered to"""
+        if isinstance(self.method, commands.Basic.Deliver):
+            return self.method.consumer_tag
+
+    @property
+    def delivery_tag(self) -> typing.Optional[int]:
+        """If the message was delivered, or retrieved via Basic.Get,
+        indicates the delivery tag value.
+
+        """
+        if isinstance(self.method, (commands.Basic.Deliver,
+                                    commands.Basic.GetOk)):
+            return self.method.delivery_tag
+
+    @property
+    def exchange(self) -> str:
+        """The exchange the message was published to"""
+        return self.method.exchange
+
+    @property
+    def routing_key(self) -> str:
+        """The routing key the message was published with"""
+        return self.method.routing_key
+
+    @property
+    def message_count(self) -> typing.Optional[int]:
+        """Return the ``message_count`` if the message was retrieved using
+        ``Basic.Get``
+
+        """
+        if isinstance(self.method, commands.Basic.GetOk):
+            return self.method.message_count
+
+    @property
+    def redelivered(self) -> typing.Optional[bool]:
+        """If the message was delivered, or retrieved via Basic.Get,
+        indicates if it was redelivered
+
+        """
+        if isinstance(self.method, (commands.Basic.Deliver,
+                                    commands.Basic.GetOk)):
+            return self.method.redelivered
+
+    @property
+    def reply_code(self) -> typing.Optional[int]:
+        """If the message was returned, includes the status code as to why"""
+        if isinstance(self.method, commands.Basic.Return):
+            return self.method.reply_code
+
+    @property
+    def reply_text(self) -> typing.Optional[str]:
+        """If the message was returned, includes the status text as to why"""
+        if isinstance(self.method, commands.Basic.Return):
+            return self.method.reply_text
+
+    @property
+    def app_id(self) -> typing.Optional[str]:
+        """Return the ``app_id`` property value"""
+        return self.header.properties.app_id
+
+    @property
+    def content_encoding(self) -> typing.Optional[str]:
+        """Return the ``content_encoding`` property value"""
+        return self.header.properties.content_encoding
+
+    @property
+    def content_type(self) -> typing.Optional[str]:
+        """Return the ``content_type`` property value"""
+        return self.header.properties.content_type
+
+    @property
+    def correlation_id(self) -> typing.Optional[str]:
+        """Return the ``correlation_id`` property value"""
+        return self.header.properties.correlation_id
+
+    @property
+    def delivery_mode(self) -> typing.Optional[int]:
+        """Return the ``delivery_mode`` property value"""
+        return self.header.properties.delivery_mode
+
+    @property
+    def expiration(self) -> typing.Optional[str]:
+        """Return the ``expiration`` property value"""
+        return self.header.properties.expiration
+
+    @property
+    def headers(self) -> typing.Optional[typing.Dict[str, typing.Any]]:
+        """Return the ``headerss`` property value"""
+        return self.header.properties.headers
+
+    @property
+    def message_id(self) -> typing.Optional[str]:
+        """Return the ``message_id`` property value"""
+        return self.header.properties.message_id
+
+    @property
+    def message_type(self) -> typing.Optional[str]:
+        """Return the ``message_type`` property value"""
+        return self.header.properties.message_type
+
+    @property
+    def priority(self) -> typing.Optional[int]:
+        """Return the ``priority`` property value"""
+        return self.header.properties.priority
+
+    @property
+    def reply_to(self) -> typing.Optional[str]:
+        """Return the ``reply_to`` property value"""
+        return self.header.properties.reply_to
+
+    @property
+    def timestamp(self) -> typing.Optional[datetime.datetime]:
+        """Return the ``timestamp`` property value"""
+        return self.header.properties.timestamp
+
+    @property
+    def user_id(self) -> typing.Optional[str]:
+        """Return the ``user_id`` property value"""
+        return self.header.properties.user_id
+
+    @property
+    def body(self) -> bytes:
+        """The message body"""
+        return b''.join([b.value for b in self.body_frames])
+
+    @property
+    def complete(self):
+        length = len(self)
+        LOGGER.debug('Body size: %r, expect %r', length, self.header.body_size)
+        return length == self.header.body_size
---
+++ tests/test_message.py
@@ -0,0 +1,142 @@
+import datetime
+import random
+import time
+import unittest
+import uuid
+
+from pamqp import body, constants, commands, header
+
+from aiorabbit import message
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.exchange = str(uuid.uuid4())
+        self.routing_key = str(uuid.uuid4())
+        self.app_id = str(uuid.uuid4())
+        self.content_encoding = str(uuid.uuid4())
+        self.content_type = str(uuid.uuid4())
+        self.correlation_id = str(uuid.uuid4())
+        self.delivery_mode = random.randint(1, 2)
+        self.expiration = str(int(time.time()) + random.randint(60, 300))
+        self.headers = {str(uuid.uuid4()): str(uuid.uuid4())}
+        self.message_id = str(uuid.uuid4())
+        self.message_type = str(uuid.uuid4())
+        self.priority = random.randint(0, 255)
+        self.reply_to = str(uuid.uuid4())
+        self.timestamp = datetime.datetime.now()
+        self.user_id = str(uuid.uuid4())
+        self.body = b'-'.join(
+            [str(uuid.uuid4()).encode('latin-1')
+             for _offset in range(0, random.randint(1, 100))])
+
+        self.message = message.Message(self.get_method())
+        self.message.header = header.ContentHeader(
+            0, len(self.body),
+            commands.Basic.Properties(
+                app_id=self.app_id,
+                content_encoding=self.content_encoding,
+                content_type=self.content_type,
+                correlation_id=self.correlation_id,
+                delivery_mode=self.delivery_mode,
+                expiration=self.expiration,
+                headers=self.headers,
+                message_id=self.message_id,
+                message_type=self.message_type,
+                priority=self.priority,
+                reply_to=self.reply_to,
+                timestamp=self.timestamp,
+                user_id=self.user_id))
+
+        value = bytes(self.body)
+        while value:
+            self.message.body_frames.append(
+                body.ContentBody(value[:constants.FRAME_MAX_SIZE]))
+            value = value[constants.FRAME_MAX_SIZE:]
+
+    def get_method(self):
+        raise NotImplemented
+
+    def compare_message(self):
+        for attribute in [
+                'exchange', 'routing_key', 'app_id', 'content_encoding',
+                'content_type', 'correlation_id', 'delivery_mode',
+                'expiration', 'headers', 'message_id', 'message_type',
+                'priority', 'reply_to', 'timestamp', 'user_id', 'body']:
+            expectation = getattr(self, attribute)
+            value = getattr(self.message, attribute)
+            if isinstance(expectation, dict):
+                self.assertDictEqual(value, expectation)
+            else:
+                self.assertEqual(value, expectation)
+        self.assertEqual(bytes(self.message), self.body)
+        self.assertEqual(len(self.message), len(self.body))
+        self.assertTrue(self.message.complete)
+
+
+class BasicDeliverTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.consumer_tag = uuid.uuid4().hex
+        self.delivery_tag = random.randint(0, 100000000)
+        self.redelivered = bool(random.randint(0, 1))
+        super().setUp()
+
+    def get_method(self):
+        return commands.Basic.Deliver(
+            self.consumer_tag, self.delivery_tag, self.redelivered,
+            self.exchange, self.routing_key)
+
+    def test_properties(self):
+        self.assertEqual(self.message.consumer_tag, self.consumer_tag)
+        self.assertEqual(self.message.delivery_tag, self.delivery_tag)
+        self.assertEqual(self.message.redelivered, self.redelivered)
+        self.assertIsNone(self.message.message_count)
+        self.assertIsNone(self.message.reply_code)
+        self.assertIsNone(self.message.reply_text)
+        self.compare_message()
+
+
+class BasicGetOkTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.delivery_tag = random.randint(0, 100000000)
+        self.message_count = random.randint(0, 1000)
+        self.redelivered = bool(random.randint(0, 1))
+        super().setUp()
+
+    def get_method(self):
+        return commands.Basic.GetOk(
+            self.delivery_tag, self.redelivered, self.exchange,
+            self.routing_key, self.message_count)
+
+    def test_properties(self):
+        self.assertEqual(self.message.delivery_tag, self.delivery_tag)
+        self.assertEqual(self.message.message_count, self.message_count)
+        self.assertEqual(self.message.redelivered, self.redelivered)
+        self.assertIsNone(self.message.reply_code)
+        self.assertIsNone(self.message.reply_text)
+        self.compare_message()
+
+
+class BasicReturnTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.reply_code = random.randint(200, 500)
+        self.reply_text = str(uuid.uuid4())
+        super().setUp()
+
+    def get_method(self):
+        return commands.Basic.Return(
+            self.reply_code, self.reply_text, self.exchange, self.routing_key)
+
+    def test_properties(self):
+        self.assertEqual(self.message.reply_code, self.reply_code)
+        self.assertEqual(self.message.reply_text, self.reply_text)
+        self.assertIsNone(self.message.consumer_tag)
+        self.assertIsNone(self.message.delivery_tag)
+        self.assertIsNone(self.message.message_count)
+        self.assertIsNone(self.message.redelivered)
+        self.compare_message()
+
